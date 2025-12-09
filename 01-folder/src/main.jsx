4fae881cb8e2fe32681bcc0af60f05ff6745e57c
imvaibhav99 <imvaibhav9796@gmail.com>
2025-12-09 11:09:54 +0530

    first commit

---
+++ 01-folder/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+const { createRoot } = ReactDOM
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+
+)
---
+++ script.js
@@ -0,0 +1,3 @@
+document.createElement('h1');
+h1.innerHTML= "Hello From Vaibhav";
+document.body.appendChild(h1);
